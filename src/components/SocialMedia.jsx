2a20a40ed8356c5f231006c2f6c7433531ddd4f2
youssefabdelmaged <youssefabdelmaged50@1gmail.com>
2024-02-17 10:48:22 +0200

    some edits

--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -1,18 +1,19 @@
 import React from "react";
-import { BsFacebook, BsInstagram, BsTwitter } from "react-icons/bs";
+import { BsInstagram, BsLinkedin ,} from "react-icons/bs";
 
 const SocialMedia = () => {
   return (
     <div className="app__social">
+        <a href="https://www.instagram.com/_mega1411/">
       <div>
         <BsInstagram />
       </div>
+      </a>
+      <a href="https://www.linkedin.com/in/youssef-abdelmaged-8b6a791bb/">
       <div>
-        <BsFacebook />
+        <BsLinkedin />
       </div>
-      <div>
-        <BsTwitter />
-      </div>
+      </a>
     </div>
   );
 };
