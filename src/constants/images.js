7c7081f10fb69fd36ef50a888324c192fb4e6dff
youssefabdelmaged <youssefabdelmaged50@1gmail.com>
2024-02-18 23:28:04 +0200

    mega

--- src/constants/images.js
+++ src/constants/images.js
@@ -34,8 +34,11 @@ import bolt from '../assets/bolt.png';
 import nb from '../assets/nb.png';
 import skype from '../assets/skype.png';
 import spotify from '../assets/spotify.png';
-import whatsapp from '../assets/whatsapp.png'
+import whatsapp from '../assets/whatsapp.png';
+
+
 export default {
+  
   email,
   mobile,
   api,
